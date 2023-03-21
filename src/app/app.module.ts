a5cccb2674bab0ba47bdae1fbfb29474d32f3a80
Abdallah Mustapha <abdullahmustapha59@gmail.com>
2023-03-21 13:22:15 +0000

    toggle btn added:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { LikePageComponent } from './like-page/like-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    LikePageComponent
   ],
   imports: [
     FormsModule,
