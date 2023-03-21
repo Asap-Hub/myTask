38ca6581bb34bd0927baf5d44cd36011da39555b
Abdallah Mustapha <abdullahmustapha59@gmail.com>
2023-03-21 16:42:50 +0000

    courses added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { LikePageComponent } from './like-page/like-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    LikePageComponent
+    LikePageComponent,
+    NavBarComponent
   ],
   imports: [
     FormsModule,
